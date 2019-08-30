2018d591f03adbe07670deef1c9690b62014d5b9
Alysse Brooks <alysse.brooks20@gmail.com>
2019-08-30 14:11:31 +0100

    websocket added and background set

--- server.rb
+++ server.rb
@@ -1,8 +1,27 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "sinatra-websocket"
 
 class Server < Sinatra::Base
-    get '/' do
-        erb :index
+  set :sockets, Array.new
+
+  get "/" do
+    erb :index
+  end
+
+  get "/socket" do
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each { |socket| socket.send(msg) } }
+      end
+
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
     end
+  end
 end
