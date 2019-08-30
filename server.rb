1f27dbfdfa58adf58f5332ac1c0ff688e6790ab6
Alysse Brooks <alysse.brooks20@gmail.com>
2019-08-30 16:14:02 +0100

    websocket connected to database

--- server.rb
+++ server.rb
@@ -1,6 +1,9 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "sinatra-websocket"
+require_relative "./lib/users.rb"
+require_relative "./lib/tasks.rb"
+require_relative "./lib/boards.rb"
 
 class Server < Sinatra::Base
   set :sockets, Array.new
@@ -9,14 +12,18 @@ class Server < Sinatra::Base
     erb :index
   end
 
-  get "/socket" do
+  get "/socket/:board_id" do |board_id|
     request.websocket do |ws|
       ws.onopen do
         settings.sockets << ws
       end
 
       ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each { |socket| socket.send(msg) } }
+        p msg
+        # EM.next_tick { settings.sockets.each { |socket| socket.send(msg) } }
+        usersname, title, description = msg.split("|")
+        user = User.create(:name => usersname)
+        Task.create(title: title, description: description, user_id: user.id, board_id: board_id)
       end
 
       ws.onclose do
